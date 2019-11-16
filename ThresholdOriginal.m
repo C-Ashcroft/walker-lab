2e81b7e754353062b385310c7b0accd71abeab64
Caitlin Ashcroft <57772139+C-Ashcroft@users.noreply.github.com>
2019-11-16 13:33:11 +0000

    Update ThresholdOriginal.m

--- ThresholdOriginal.m
+++ ThresholdOriginal.m
@@ -1,6 +1,6 @@
 function ThresholdOriginal
 
-names = {'will';'seb';'noah';'leonie'; 'iris';'gemma';'dom'; 'chris'; 'caitlin';'adam'};
+names = {'CA1';'CA2';'CA3';'CA4'; 'CA5';'CA6';'CA7'; 'CA8'};
 
 
 allsubjectspercentcorrect = [];
